2d82166fc2cedba7bb181d965c0251d5674aadc8
Jacek Gizinski <jacektrg+github@gmail.com>
2024-07-30 19:59:12 +0200

    Adding route creating new chat room.

---
+++ routes/api.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { nanoidCustom } = require('../lib/utils');
+const { roomList } = require('../socketio/socketio');
+
+router.get('/create-room', (req, res, next) => {
+	let newRoomId = nanoidCustom();
+	if (!roomList.length) {
+		const newRoom = {
+			id: newRoomId,
+			userList: [],
+			messageList: [],
+			videoList: [],
+		};
+		roomList.push(newRoom);
+		return res.status(200).json({ roomId: newRoomId });
+	} else {
+		while (!roomList.find((room) => room.id === newRoomId)) {
+			newRoomId = nanoidCustom();
+		}
+		return res.status(200).json({ roomId: newRoomId });
+	}
+});
+
+module.exports = router;
