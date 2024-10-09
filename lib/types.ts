855d13e991f37c2926a882d28fe4aacbc5a5b44e
Jacek Gizinski <jacektrg+github@gmail.com>
2024-10-09 20:01:28 +0200

    Updating socket events.

--- lib/types.ts
+++ lib/types.ts
@@ -38,11 +38,14 @@ type ServerToClientEvents = {
 	oops: (error: any) => void;
 	all_room_data: (data: AllRoomData) => void;
 	new_user_joined: (userData: UserDataClient) => void;
-	new_message_received: (messageData: MessageData) => void;
+	new_message_received: (messageData: MessageData[]) => void;
 	new_video_added: (videoData: VideoData[]) => void;
+	video_was_removed: (videoData: VideoData[]) => void;
+	// ! Unfinished
 	start_video_playback: () => void;
 	stop_video_playback: () => void;
-	jump_to_video_time: (seconds: number) => void;
+	video_seek: (seconds: number) => void;
+	change_video: (data: { roomId: string; videoId: string }) => void;
 	video_ended: (data: { roomId: string; time: number }) => void;
 	error: (message: string) => void;
 };
@@ -50,11 +53,13 @@ type ServerToClientEvents = {
 type ClientToServerEvents = {
 	send_message: (data: { roomId: string; message: string }) => void;
 	add_video: (data: { roomId: string; video: VideoData }) => void;
+	remove_video: (data: { roomId: string; video: VideoData }) => void;
+	// ! Unfinished
 	start_video_playback: (roomId: string) => void;
 	stop_video_playback: (roomId: string) => void;
-	jump_to_video_time: (data: { roomId: string; time: number }) => void;
+	video_seek: (data: { roomId: string; time: number }) => void;
 	change_video: (data: { roomId: string; videoId: string }) => void;
-	video_ended: (roomId: string) => void;
+	video_ended: (data: { roomId: string; videoId: string }) => void;
 };
 
 export type Hosts = 'youtube';
