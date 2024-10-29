ea0a6b8a6ad3b52eab9989a9d9c671b82940e20a
Jacek Gizinski <jacektrg+github@gmail.com>
2024-10-29 20:12:37 +0100

    Removing redundant room creation which is handled by socket.io logic.

--- routes/api.ts
+++ routes/api.ts
@@ -10,19 +10,11 @@ export default router.get('/create-room', (res: Response) => {
 	const { room_list } = global;
 	let newRoomId = nanoidCustom();
 	if (!room_list.length) {
-		const newRoom: RoomData = {
-			id: newRoomId,
-			createdAt: Date.now(),
-			userList: [],
-			messageList: [],
-			videoList: [],
-		};
-		room_list.push(newRoom);
-		return res.status(200).json({ roomId: newRoomId });
+		return res.status(200).json(newRoomId);
 	} else {
 		while (room_list.find((room) => room.id === newRoomId)) {
 			newRoomId = nanoidCustom();
 		}
-		return res.status(200).json({ roomId: newRoomId });
+		return res.status(200).json(newRoomId);
 	}
 });
