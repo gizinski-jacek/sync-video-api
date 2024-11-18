96c04c2c85d1a5701b3d9650476c0f604efac2ae
Jacek Gizinski <jacektrg+github@gmail.com>
2024-11-18 20:22:49 +0100

    Adding welcome page response for /api route.
    Adding missing req parameter in route handler.
    Fixing typescript error by removing return statement when sending response.

--- routes/api.ts
+++ routes/api.ts
@@ -1,20 +1,30 @@
-import express, { Response } from 'express';
-const router = express.Router();
+import express, { Request, Response } from 'express';
 import { nanoidCustom } from '../lib/utils';
-import { RoomData } from '../lib/types';
 import { global } from '../lib/global';
 
-// ! Find the reason for this error
-// @ts-ignore
-export default router.get('/create-room', (res: Response) => {
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+	res.send(
+		`<main>
+				<h2>Sync Video API.</h2>
+				<p><a href=https://github.com/gizinski-jacek/sync-video-api>Express API Github Repository</a></p>
+				<p><a href=https://github.com/gizinski-jacek/sync-video>NextJS Client Github Repository</a></p>
+			</main>`
+	);
+});
+
+router.get('/create-room', (req: Request, res: Response) => {
 	const { room_list } = global;
 	let newRoomId = nanoidCustom();
 	if (!room_list.length) {
-		return res.status(200).json(newRoomId);
+		res.status(200).json({ roomId: newRoomId });
 	} else {
 		while (room_list.find((room) => room.id === newRoomId)) {
 			newRoomId = nanoidCustom();
 		}
-		return res.status(200).json(newRoomId);
+		res.status(200).json({ roomId: newRoomId });
 	}
 });
+
+export default router;
