6f0645975f740b11fa0af6b34a389c18cfe03d68
Jacek Gizinski <jacektrg+github@gmail.com>
2024-11-18 20:23:06 +0100

    Adding types.

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const router = express.Router();
 
 /* GET home page. */
-export default router.get('/', (req, res, next) => {
+export default router.get('/', (req: Request, res: Response) => {
 	res.redirect('/api');
 });
