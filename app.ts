87a8c6230fd8d4887a0e4a591d68606286375e1c
Jacek Gizinski <jacektrg+github@gmail.com>
2024-12-16 17:19:33 +0100

    Updating types

--- app.ts
+++ app.ts
@@ -45,7 +45,7 @@ app.use('/api', apiRouter);
 cleanupRooms();
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
 	next(createError(404));
 });
 
